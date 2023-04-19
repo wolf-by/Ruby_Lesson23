3d1d9f2282d99e3728714f0fe4ab718a6468e05a
wolf-by <wolf.by@yandex.ru>
2023-04-19 17:54:07 +0300

    Add - selected, options

--- app.rb
+++ app.rb
@@ -4,7 +4,7 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 get '/' do
-	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School !</a>"			
+	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School - Labs 23</a>"			
 end
 
 get '/about' do 
